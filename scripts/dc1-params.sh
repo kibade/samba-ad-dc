1ff04faf7c9a685c032aaf2dcac4cf73d8a09aa6
Steven Monai <steve-gh@monai.ca>
2017-06-29 09:33:05 -0700

    dc1-params.sh Removed REV_DNS_ZONE
    Changes committed:
    	modified:   dc1-params.sh

--- scripts/dc1-params.sh
+++ scripts/dc1-params.sh
@@ -8,9 +8,6 @@ REALM="SFG.AD.SD57.BC.CA"
 ADMIN_PASSWORD="secRet!23"
 HOSTNAME="dc1"
 NTP_SERVER1="time.sd57.bc.ca"
-REV_DNS_ZONE="10.45.10.in-addr.arpa"
-
-HOST_NUM=$(echo ${IP_ADDRESS} | cut -f4 -d.)
 
 if which smbd >&/dev/null; then
 CONFIGFILE=$(smbd -b |egrep CONFIGFILE |cut -f2- -d':' |sed 's/^ *//')
